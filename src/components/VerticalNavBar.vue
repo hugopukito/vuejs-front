e97540d13bc6c87b673f1c43a5cb44eed30b1f12
hugopukito <gogo26hugop@gmail.com>
2022-08-13 22:14:25 +0200

    vertical nav bar working

--- src/components/VerticalNavBar.vue
+++ src/components/VerticalNavBar.vue
@@ -1,38 +1,45 @@
 <template>
   <nav>
-    <ul>
-      <li><a href="#1st">1st</a></li>
-      <li><a href="#2nd">2nd</a></li>
-      <li><a href="#3rd">3rd</a></li>
-    </ul>
-  </nav>
+  <ul>
+    <li><a href="#section-1">Section 1</a></li>
+    <li><a href="#section-2">Section 2</a></li>
+    <li><a href="#section-3">Section 3</a></li>
+    <li><a href="#section-4">Section 4</a></li>
+    <li><a href="#section-5">Section 5</a></li>
+    <li><a href="#section-6">Section 6</a></li>
+    <li><a href="#section-7">Section 7</a></li>
+    <li><a href="#section-8">Section 8</a></li>
+    <li><a href="#section-9">Section 9</a></li>
+  </ul>
+</nav>
 </template>
+
+<script setup>
+  
+</script>
 
 <style scoped>
 nav {
   white-space: nowrap;
-  background: blanchedalmond;
-  width: 170px;
-  height: 100vh;
-  top: 0;
-  position: sticky;
-  float: left;
+  background: #37474F;
+  width: 150px;
 }
-
 nav ul {
-  list-style-type: none;
+  list-style: none;
   margin: 0;
   padding: 0;
 }
-
+nav ul {
+  position: sticky;
+  top: 0;
+}
 nav ul li a {
   display: block;
   padding: 0.5rem 1rem;
   color: white;
   text-decoration: none;
 }
-
-a:hover {
-  background-color: rgb(161, 3, 3);
+nav ul li a.current {
+  background: black;
 }
 </style>
--- src/views/Topics.vue
+++ src/views/Topics.vue
@@ -1,53 +1,77 @@
 <template>
+  <div class="grid">
     <vertical-nav-bar/>
-    <h1> topics </h1>
-    <main>
-      <section id="section-1">
-        <h2>Section 1</h2>
+    <div class="main">
+      <div class="h1"> topics </div>
+      <div class="section" id="section-1">
+          <div class="h2">Section 1</div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, blanditiis expedita? Earum eligendi pariatur quaerat quos expedita ab quibusdam ratione veniam in, mollitia fuga repudiandae?
+      </div>
+      <div class="section" id="section-2">
+        <div class="h2">Section 2</div>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, blanditiis expedita? Earum eligendi pariatur quaerat quos expedita ab quibusdam ratione veniam in, mollitia fuga repudiandae?</p>
-      </section>
-      <section id="section-2">
-        <h2>Section 2</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, blanditiis expedita? Earum eligendi pariatur quaerat quos expedita ab quibusdam ratione veniam in, mollitia fuga repudiandae?</p>
-      </section>
+      </div>
       <section id="section-3">
-        <h2>Section 3</h2>
+        <div class="h2">Section 3</div>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, blanditiis expedita? Earum eligendi pariatur quaerat quos expedita ab quibusdam ratione veniam in, mollitia fuga repudiandae?</p>
       </section>
       <section id="section-4">
-        <h2>Section 4</h2>
+        <div class="h2">Section 4</div>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, blanditiis expedita? Earum eligendi pariatur quaerat quos expedita ab quibusdam ratione veniam in, mollitia fuga repudiandae?</p>
       </section>
       <section id="section-5">
-        <h2>Section 5</h2>
+        <div class="h2">Section 5</div>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, blanditiis expedita? Earum eligendi pariatur quaerat quos expedita ab quibusdam ratione veniam in, mollitia fuga repudiandae?</p>
+        <br><br><br><br><br><br><br><br>
       </section>
       <section id="section-6">
-        <h2>Section 6</h2>
+        <div class="h2">Section 6</div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, blanditiis expedita? Earum eligendi pariatur quaerat quos expedita ab quibusdam ratione veniam in, mollitia fuga repudiandae?</p>
+        <br><br><br><br><br><br><br><br>
+      </section>
+      <section id="section-7">
+        <div class="h2">Section 7</div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, blanditiis expedita? Earum eligendi pariatur quaerat quos expedita ab quibusdam ratione veniam in, mollitia fuga repudiandae?</p>
+        <br><br><br><br><br><br><br><br>
+      </section>
+      <section id="section-8">
+        <div class="h2">Section 8</div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, blanditiis expedita? Earum eligendi pariatur quaerat quos expedita ab quibusdam ratione veniam in, mollitia fuga repudiandae?</p>
+        <br><br><br><br><br><br><br><br>
+      </section>
+      <section id="section-9">
+        <div class="h2">Section 9</div>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, blanditiis expedita? Earum eligendi pariatur quaerat quos expedita ab quibusdam ratione veniam in, mollitia fuga repudiandae?</p>
       </section>
-    </main>
+    </div>
+    <footer>&copy; pukito</footer>
+  </div>
 </template>
 
-<script setup>
-import VerticalNavBar from "@/components/VerticalNavBar.vue"
-</script>
-
-
 <style scoped>
-h1{
-  margin-top: 30px;
-  /* padding-right: 200px; */
+.grid {
+  margin: 0;
+  display: grid;
+  grid-template-columns: min-content;
+}
+
+.h1 {
   background-color: blueviolet;
+  margin-top: 0;
 }
 
-h2{
-  /* padding-right: 200px; */
+.section {
   background-color:brown;
+  margin-top: 0;
 }
 
-section {
-  text-align: left;
+footer {
+  background-color:coral;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  grid-column: 1 / 3;
 }
 
 @media screen and (max-device-width: 480px) {
@@ -57,3 +81,26 @@ section {
   }
 }
 </style>
+
+<script setup>
+import VerticalNavBar from "@/components/VerticalNavBar.vue"
+
+let mainNavLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
+</script>
