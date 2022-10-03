acfbf7b52c1fc88dfa664a4f6e9c8f3af6b47fb5
pukito <gogo26hugop@gmail.com>
2022-10-03 17:59:36 +0200

    signup and signin working

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,3 +1,24 @@
 <template>
-    signin
-</template>
+    <input @keyup.enter="submit" v-model="email" placeholder="email"/> <br>
+    <input @keyup.enter="submit" v-model="password" placeholder="password"/> <br>
+    <button @click="submit">Envoyer</button>
+  </template>
+  
+  <script setup>
+  import { ref } from "vue";
+  import axios from "axios"
+  
+  let email = ref("")
+  let password = ref("")
+  
+  async function submit() {
+      const obj = {
+        "email": email.value,
+        "password": password.value
+      };
+      await axios.post(process.env.VUE_APP_API_URL + "/signin", JSON.stringify(obj))
+      .then(resp => {
+        console.log(resp.data.token);
+      })
+  }
+  </script>
