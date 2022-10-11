02bce8def259b0157ea17e9dac36903b46e5de56
pukito <gogo26hugop@gmail.com>
2022-10-11 23:13:22 +0200

    websocket, removed http chat

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,91 +1,30 @@
 <template>
-    <div v-if="userName === null">
-        <h3> Create an account and Login to put your message ! </h3>
-    </div>
-    <div v-else>
-        <Input @submit="createMsg" />
-    </div>
-    <div v-if = isLoading><h1>loading...</h1></div>
-    <div class="msgs" v-else>
-    <div class="msg" v-for="(item, index) in msg" :key="index">
-        <div class="element">
-            <div class="name"> {{ msg[msg.length-1-index].name }} </div>
-        </div>
-        <div class="element">
-            <div class="message"> {{ msg[msg.length-1-index].message }} </div>
-        </div>
-    </div>
-    </div>
+    <form :action="sendMessage" @click.prevent="onSubmit">
+        <input v-model="message" type="text">
+        <input type="submit" value="Send" @click="sendMessage">
+    </form>
+    <h3>websocket messages</h3>
+    <p>{{ rcvMessage }}</p>
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue";
 
-import { ref, onMounted, inject } from "vue"
-import axios from "axios"
-import Input from "@/components/Input.vue"
-
-let isLoading = ref(true);
-let msg = ref(String);
-let userName = ref("");
+let message = ref("")
+let socket = ref(null)
+let rcvMessage = ref("")
 
 onMounted(() => {
-    getMsg();
-    refresh();
+    socket = new WebSocket("ws://localhost:8080/socket")
+    socket.onmessage = (msg) => {
+        rcvMessage.value = msg.data
+    }
 })
 
-async function getMsg () {
-    await axios.get(process.env.VUE_APP_API_URL + "/messages")
-    .then(resp => {
-        msg.value = resp.data
-        isLoading.value = false
-    })
-}
-
-function refresh() {
-    setInterval(function() {
-        getMsgRefresh()
-    }, 10000)
-}
-
-async function getMsgRefresh() {
-    await axios.get(process.env.VUE_APP_API_URL + "/messages")
-    .then(resp => {
-        msg.value = resp.data
-    })
-}
-
-async function createMsg(message) {
-    const obj = {
+function sendMessage() {
+    let msg = {
         "message": message.value
-    };
-    await axios.post(process.env.VUE_APP_API_URL + "/messages", JSON.stringify(obj), {
-        headers: {
-            Authorization: "Bearer " + localStorage.getItem("token")
-        }
-    }).then(getMsgRefresh())
+    }
+    socket.send(JSON.stringify(msg))
 }
 </script>
-
-<style scoped>
-h1 {
-    color: aquamarine;
-}
-.msgs{
-    padding-top: 40px;
-}
-.msg{
-    padding-top: 20px;
-    font-size: 30px;
-    color:chocolate;
-}
-.element{
-    display: inline-block;
-    margin: 10px;
-}
-.name{
-    color: aquamarine;
-}
-.message{
-    color: orange;
-}
-</style>
