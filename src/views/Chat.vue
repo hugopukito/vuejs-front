e0447b4d11500c65fcb531a24db11318d2f5ff8c
pukito <gogo26hugop@gmail.com>
2022-10-16 21:15:48 +0200

    websocket working

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -3,10 +3,15 @@
         <input v-model="message" type="text">
         <input type="submit" value="Send" @click="sendMessage">
     </form>
-    <h3>websocket messages</h3>
-    <div v-for="(item, index) in rcvMessage" :key="index">
-        {{ rcvMessage[rcvMessage.length-1-index] }}
+    <div class="h3">Websocket chat</div>
+    <div v-if="!isLoading">
+        <div v-for="(_, index) in msgs" :key="index">
+            <div style="color: aquamarine;">{{ msgs[msgs.length-1-index].name }}</div>
+            <div style="color: orange;">{{ msgs[msgs.length-1-index].message }}</div>
+            <br>
+        </div>
     </div>
+    <div v-else>loading...</div>
 </template>
 
 <script setup>
@@ -14,19 +19,33 @@ import { ref, onMounted } from "vue";
 
 let message = ref("")
 let socket = ref(null)
-let rcvMessage = ref([""])
+let msgs = []
+let isLoading = ref(true)
 
 onMounted(() => {
-    socket = new WebSocket("ws://localhost:8080/socket")
+    socket = new WebSocket("ws://localhost:8080/websocket")
     socket.onmessage = (msg) => {
-        rcvMessage.value.push(msg.data)
+        isLoading.value = true
+        msgs.push(JSON.parse(msg.data))
+        isLoading.value = false
     }
 })
 
 function sendMessage() {
     let msg = {
+        "name": "golang :)",
         "message": message.value
     }
     socket.send(JSON.stringify(msg))
+    message.value = ""
 }
 </script>
+
+<style scoped>
+form {
+    margin-top: 20px;
+}
+.h3 {
+    margin-top: 20px;
+}
+</style>
