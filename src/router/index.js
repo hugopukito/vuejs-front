213a8b7b334ce536630847e5d25bf9f2702a02c1
hugopukito <gogo26hugop@gmail.com>
2022-10-17 18:27:58 +0200

    img retrieve

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Monitoring from "@/views/Monitoring.vue"
 import Chat from "@/views/Chat.vue"
 import NotFound from "@/views/NotFound.vue"
 import Keyboard from "@/views/Keyboard.vue"
+import Test from "@/views/Test.vue"
 
 const routes = [
     {
@@ -45,6 +46,14 @@ const routes = [
         component: Chat,
         meta: {
             title: "chat"
+        }
+    },
+    {
+        name: "Test",
+        path: "/test",
+        component: Test,
+        meta: {
+            title: "test"
         }
     },
     {
