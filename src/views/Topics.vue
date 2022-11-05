1342a298b3fc1e3e4c2f7c0f08f4e2efc0e96fcb
hugopukito <gogo26hugop@gmail.com>
2022-11-05 15:13:01 +0100

    finished nginx in topics

--- src/views/Topics.vue
+++ src/views/Topics.vue
@@ -52,7 +52,7 @@
 
           <div class="text">
             Os should be installed on your pi4, go on the top of your screen and select your wifi. Open a terminal and run
-            the simple command <code>ip a</code> to get the local ip address of your pi4 to access it later.
+            the simple command <div class="code">ip a</div> To get the local ip address of your pi4 to access it later.
           </div>
 
           <img src="@/assets/topics/ip_address.png">
@@ -68,10 +68,10 @@
           Now you've configured your pi4 on your local network, you can connect threw it with the SSH (Secure Shell), in the
           previous step we enable SSH so you don't need to install it manually. <br> <br>
           Notice that you can unplug all device on your pi4 (don't need a screen, mouse and keyboard anymore), you will be able 
-          to dev on it by accessing to the same terminal of your real pi4 machine with SSH protocol. Use the command bellow :<br> <br>
-          <code> ssh 'pi4_host_name'@'pi4_ip_address'</code> <br> <br>
-          In my case : <br> <br>
-          <code> ssh pukito@192.168.0.27</code> <br> <br>
+          to dev on it by accessing to the same terminal of your real pi4 machine with SSH protocol. Use the command bellow :
+          <div class="code"> ssh 'pi4HostName'@'pi4Ip'</div>
+          In my case :
+          <div class="code"> ssh pukito@192.168.0.27</div>
           Once you've hit enter, the terminal should ask you for the password you set before. <br> <br>
           You can now dev on your pi4 from another device connected to your local network ! ✨
         </div>
@@ -83,24 +83,24 @@
           not anyone can access it (although the pi4 is only visible on your local network for the moment) 🙃 <br> <br>
           The solution is SSH keys, you will generate a pair of key with your pi4, one public that will be store on your pi4, the other
           one that you will keep secret and store on the device which will access the pi4. <br> <br>
-          To generate those keys, you will need to use the command <code> ssh-keygen</code>. <br> <br>
+          To generate those keys, you will need to use the command : <div class="code"> ssh-keygen</div>
           Then they will ask you a file to save the public and private key, just press Enter. You will need to enter a passphrase, it's up to you,
           if you feel like your private key won't be stolen, just press Enter. If you set a passphrase, each time you will try to connect
           with ssh on your pi4, it will ask you the passphrase. <br> <br>
           Setting a passphrase will add another layer of security, brut forcing rsa keys need a lot of ressource so an attacker won't
           try to brut force your private key, but if he steals your private key, the passphrase will slow down the attacker. <br> <br>
-          Now you've generated the two keys in <code>/home/'pi4_host_name'/.ssh/</code>, public key: id_rsa.pub, private key: id_rsa, 
+          Now you've generated the two keys in <div class="code">/home/'pi4HostName'/.ssh/</div>, public key: id_rsa.pub, private key: id_rsa, 
           take the private key with a secure way (best way is to store it in an usb key). <br> <br>
           Next step is to go on the machine you wanna use to connect on your pi4, take your private key (id_rsa) and place it in
-          <code>/home/'your_machine_name'/.ssh/</code>. <br> <br>
-          Try to connect on your pi4 with <code>ssh 'pi4_host_name'@'pi4_ip_address'</code>, it will ask you your passphrase (if you set one),
+          <div class="code">/home/'yourMachineName'/.ssh/</div>
+          Try to connect on your pi4 with : <div class="code">ssh 'pi4HostName'@'pi4Ip'</div> It will ask you your passphrase (if you set one),
           there is one more thing to change on your pi4, if someone don't have any private key on their machine they use to connect on your pi4,
           they will use automatically your basic authentication. <br> <br>
           So you need to disable it to anyone that doesn't have the key can't use the basic
           authentication. <br> <br>
-          Go on your pi4, use <code> sudo nano /etc/ssh/sshd_config</code> and set <code> PasswordAuthentication no</code> <br> <br>
-          Now you can restart ssh service and your done ! <br> <br> 
-          <code> sudo service ssh restart</code>
+          Go on your pi4, use <div class="code"> sudo nano /etc/ssh/sshd_config</div> And set <div class="code"> PasswordAuthentication no</div>
+          Now you can restart ssh service and your done !
+          <div class="code"> sudo service ssh restart</div>
         </div>
       </section>
       <section id="port_forwarding">
@@ -114,16 +114,16 @@
           Go on the section "port forwarding". <br> <br>
           Now you need to enter the port you want to forward, in our case, the port '22' for ssh. Then select "both protocol" and put pi4 
           local ip address. <br> <br>
-          Try to open a terminal on your local machine, instead of <code>ssh 'pi4_host_name'@'pi4_ip_address'</code> try using <br> <br>
-          <code> ssh 'pi4_host_name'@'your_internet_box_ip_address'</code> <br> <br>
+          Try to open a terminal on your local machine, instead of <div class="code">ssh 'pi4HostName'@'pi4Ip'</div> Try using
+          <div class="code"> ssh 'pi4HostName'@'yourBoxIp'</div>
           You should be able to access your pi4 with another internet connection and access it from anywhere ✨ <br> <br>
           You can forward other ports, so everything which is running on a certain port locally on your pi4 can be access from anywhere ! <br> <br>
-          If you're running an api on port 8080, forward this port, put pi4 local ip address, then type 'your_internet_box_ip_address':8080 and
+          If you're running an api on port 8080, forward this port, put pi4 local ip address, then type 'yourBoxIp':8080 and
           you will be able to access your api running on your pi4 without being connected on your local network 🙃 <br> <br>
-          If you type 'your_internet_box_ip_address' without specifying any port behind, it gonna use the port 80. <br> <br>
+          If you type 'yourBoxIp' without specifying any port behind, it gonna use the port 80. <br> <br>
           Forward the port 80 of your box on the port 80 of your pi4, run a little program on you pi4 like "node.js" on port 80 
           and provide an index.html file. <br> <br>
-          Type 'your_internet_box_ip_address', you should see your index.html 😋
+          Type 'yourBoxIp', you should see your index.html 😋
         </div>
       </section>
       <section id="dns">
@@ -141,7 +141,7 @@
 
         <div class="text">
           Replace the hidden value by your box internet ip address and "hugopukito.com" by your domain name <br> <br>
-          Now you can try on your local machine <code> ssh 'pi4_host_name'@'your_domain_name'</code> <br> <br>
+          Now you can try on your local machine <div class="code"> ssh 'pi4HostName'@'yourDomainName'</div>
           If you type your domain name on google's search bar, you should see your fresh index.html 🧊 <br> <br>
           You don't need anymore your box internet ip address and can give your domain name to your friends 😋
         </div>
@@ -150,66 +150,84 @@
         <div class="h2">NGINX 🛣️</div>
         <div class="text">
           By accessing your ip with your DNS, you get on the port 80 by default, if you want to access a service
-          running on another port, for example port 8080, you need to type 'your_dns':8080, you also need to
+          running on another port, for example port 8080, you need to type 'yourDns':8080, you also need to
           add port 8080 on port forwarding on your internet box configuration, it can be pretty anoying if you
           want to add different services on your pi4. <br> <br>
           Thanks to a reverse proxy, you can do some routing on your DNS ! <br> <br>
           Imagine you're running your golang api on localhost:8080, you want to access it with your DNS by typing
-          'your_dns'/api (for example). <br> <br>
+          'yourDns'/api (for example). <br> <br>
           By running NGINX on your pi4, you will be able to configure all 
           routes to go on your local services. <br> <br>
           NGINX will run on your port 80 by default, you will first configure the default route to give your
           index.html 🧊. So when a user will type your DNS, it will go on your port 80, then go on your NGINX
-          and will route to your index.html. <br> <br>
-          <code>sudo apt install nginx</code> <br> <br>
-          Now you need to go in nginx directory and create your own conf file for your website. <br> <br> 
-          <code>cd /etc/nginx/sites-enabled/</code> <br> <br> 
-          <code>cd /etc/nginx/sites-available/</code> <br> <br> 
-          Remove everything in those folders, create a file named with your DNS (for me it's hugopukito.com) <br> <br>
-          <code>cd /etc/nginx/sites-enabled/ && touch 'your_dns'</code> <br> <br>
-          And create a link between sites-enabled and sites-available so you don't need to duplicate this file. <br> <br>
-          <code>ln -s /etc/nginx/sites-enabled/'your_dns' /etc/nginx/sites-available/</code> <br> <br>
-          🚧 In progress... 🚧
-          <!-- Now everything you add in this file (mine is hugopukito.com) will be used by nginx as a conf file, 
+          and will route to your index.html.
+          <div class="code">sudo apt install nginx</div>
+          Now you need to go in nginx directory and create your own conf file for your website.
+          <div class="code">cd /etc/nginx/sites-enabled/</div>
+          <div class="code">cd /etc/nginx/sites-available/</div>
+          Remove everything in those folders, create a file named with your DNS (for me it's hugopukito.com).
+          <div class="code">cd /etc/nginx/sites-enabled/ && touch 'yourDns'</div>
+          And create a link between sites-enabled and sites-available so you don't need to duplicate this file.
+          <div class="code">ln -s /etc/nginx/sites-enabled/'yourDns' /etc/nginx/sites-available/</div>
+          Now everything you add in this file (mine is hugopukito.com) will be used by nginx as a conf file, 
           you can now put your index.html like so :
-          <pre>
-          <code style="display:block;">http {
-            server {
-              listen 80;
-              location / 
-                {
-                  root /www/data;
-                }
-              }
-            }</code>
-          </pre> -->
+          <div class="code" style="text-align:left;">
+            server { <br>
+              &ensp; listen 80; <br>
+              &ensp; location / { <br>
+              &ensp; &ensp; root /home/'yourWorkDir'/website; <br> 
+              &ensp;&ensp;} <br>
+            } <br>
+          </div>
+          Go in your working directory and create a folder 'website' with the index.html in it,
+          then you need to reload nginx and you type your dns.
+          <div class="code">sudo systemctl reload nginx</div>
+          You should see your index.html 🧊 like before ! 😋 <br> <br>
+          Let's return to our basic example, we want to add a route to our service running on port 8080. <br> <br>
+          This service running on port 8080 is our api, so we will add a route in the nginx conf for it.
+          <div class="code" style="text-align:left;">
+            server { <br>
+              &ensp; listen 80; <br> <br>
+              &ensp; location / { <br>
+              &ensp; &ensp; root /home/'yourWorkDir'/website; <br> 
+              &ensp;&ensp;} <br> <br>
+              &ensp; location /api { <br>
+              &ensp; &ensp; proxy_pass http://127.0.0.1:8080; <br> 
+              &ensp;&ensp;} <br>
+            } <br>
+          </div>
+          We add the route /api in our conf, '127.0.0.1' refer to 'localhost'. The proxy pass will pass
+          all request on our DNS with /api on our service running on local on port 8080. <br> <br>
+          Personnally, I need to type 'hugopukito.com/api' to access my api running locally on port 8080 <br> <br>
+          Try typing 'yourDns'/api, you should see the response from your api printing ! 🐯 <br> <br>
+          You can create as many services as you want and route then with NGINX ✨
         </div>
       </section>
       <section id="https">
         <div class="h2">HTTPS 🔒</div>
-        <div class="text">
+        <div class="text" style="text-align:center;">
           🚧 In progress... 🚧
         </div>
       </section>
       <section id="monitoring">
         <div class="h2">Monitoring 📺</div>
         <div class="h3">Grafana</div>
-        <div class="text">
+        <div class="text" style="text-align:center;">
           🚧 In progress... 🚧
         </div>
         <div class="h3">Telegraf</div>
-        <div class="text">
+        <div class="text" style="text-align:center;">
           🚧 In progress... 🚧
         </div>
       </section>
       <section id="chat">
         <div class="h2">Chat 🗣️</div>
         <div class="h3">Back-end</div>
-        <div class="text">
+        <div class="text" style="text-align:center;">
           🚧 In progress... 🚧
         </div>
         <div class="h3">Front-end</div>
-        <div class="text">
+        <div class="text" style="text-align:center;">
           🚧 In progress... 🚧
         </div>
       </section>
@@ -231,7 +249,6 @@
 }
 
 section {
-  /* background-color: crimson; */
   min-height: 100px;
   width: 60vw;
   margin: 0 auto;
@@ -245,19 +262,24 @@ img {
   box-shadow: 0px 0px 30px 3px rgba(212, 215, 135, 0.15);
 }
 
+.code {
+  background-color: #1e1e3f;
+  color: #9efeff;
+  padding: 10px;
+  border-radius: 10px;
+  font-family: monospace;
+  margin: 20px 0;
+  text-align: start;
+}
+
 .text {
-  font-size: 1.1em;
+  font-size: 1.2em;
   margin-top: 3em;
   margin-bottom: 3em;
   width: 50vw;
   margin-left: auto;
   margin-right: auto;
-}
-
-code {
-  background-color: #111;
-  color: rgb(0, 153, 0);
-  padding: 1px;
+  text-align: start;
 }
 
 .close_button {
@@ -305,7 +327,7 @@ nav ul li:hover, nav ul li.active {
     margin-top: 1.5em;
   }
   section, img, .text {
-    width: 95vw;
+    width: 90vw;
     margin-left: auto;
     margin-right: auto;
   }
