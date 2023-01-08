3742a6929f305901c01ee984cc8ca3c6005efac0
hugopukito <gogo26hugop@gmail.com>
2023-01-08 15:01:39 +0100

    static files generated

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const PrerenderSPAPlugin = require('prerender-spa-plugin-next')
+
+module.exports = {
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        routes: [ '/', '/topics', '/keyboard' ],
+      })
+    ]
+  }
+}
